4b92e87337e30587c52c942dda3241d900ce0925
Dima Motpan <dumitru.motpan@gmail.com>
2019-12-01 23:17:21 +0200

    Solve day1

--- days/day1/index.js
+++ days/day1/index.js
@@ -1,16 +1,39 @@
 let path = require('path');
 let Day = require(path.join(__dirname, '..', '..', 'helpers', 'Day'));
-let isTest = true;
+let isTest = false;
 
 const day1 = new Day(1, isTest);
 day1.task(1, task1);
 day1.task(2, task2);
 
-
 function task1(data) {
-  return data;
+  const numbers = data.split('\r\n').filter(n => n).map(n => +n);
+
+  return numbers.reduce((result, mass) => {
+    const fuel = Math.floor(mass / 3) - 2;
+    return result + fuel;
+  }, 0);
 }
 
 function task2(data) {
-  return data;
+  const numbers = data.split('\r\n').filter(n => n).map(n => +n);
+
+  return numbers.reduce((result, mass) => {
+    const totalFuel = getRecursiveFuel(mass);
+    return result + totalFuel;
+  }, 0);
 }
+
+function getRecursiveFuel(mass) {
+  let totalFuel = 0;
+  let fuel = mass;
+
+  while (fuel > 0) {
+    let newFuel = Math.floor(fuel / 3) - 2;
+    fuel = newFuel < 0 ? 0 : newFuel;
+    totalFuel += fuel;
+  }
+
+  return totalFuel;
+}
+
