27d0aba5dfe1b5ba5ee26eec1dfaa3a5674aa6ff
Dima Motpan <dumitru.motpan@gmail.com>
2019-12-11 09:16:38 +0200

    Solve day11

--- helpers/matrix.js
+++ helpers/matrix.js
@@ -24,6 +24,30 @@ function printSet(set) {
   return Array.from(set.values()).join(' ');
 }
 
+function printGraphMatrix(matrix, separator = '#') {
+  let maxX = 0;
+  let maxY = 0;
+  let minX = Number.MAX_SAFE_INTEGER;
+  let minY = Number.MAX_SAFE_INTEGER;
+
+  Object.keys(matrix).forEach(key => {
+    let [x, y] = key.split(separator).map(n => +n);
+    maxX = Math.max(x, maxX);
+    minX = Math.min(x, minX);
+    maxY = Math.max(y, maxY);
+    minY = Math.min(y, minY);
+  });
+
+  for (let y = maxY; y >= minY; y--) {
+    for (let x = maxX; x >= minX; x--) {
+      process.stdout.write(matrix[`${x}${separator}${y}`] || ' ')
+    }
+    console.log();
+  }
+}
+
+
 module.exports.matrixOfZeros = matrixOfZeros;
 module.exports.matrixOfChars = matrixOfChars;
 module.exports.prettyPrintMatrix = prettyPrintMatrix;
+module.exports.printGraphMatrix = printGraphMatrix;
