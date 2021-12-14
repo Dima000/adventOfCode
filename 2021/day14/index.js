f2dc09a75bf817a6fadc25ddbd80864004b19943
Dumitru <dumitru.motpan@gmail.com>
2021-12-14 22:12:06 +0200

    2021 day 15 - improvements to readability

--- 2021/day14/index.js
+++ 2021/day14/index.js
@@ -11,20 +11,22 @@ function task1(data) {
     const rules = new Map();
     let counts = new Map();
 
+    // populate rules
     lines.split('\r\n').forEach(line => {
         const [key, elem] = line.split(' -> ');
         rules.set(key, [`${key[0]}${elem}`, `${elem}${key[1]}`]);
         counts.set(key, 0);
     });
 
+    // populate initial groups count
     for (let i = 0; i < start.length - 1; i++) {
         const current = start[i] + start[i + 1];
         counts.set(current, counts.get(current) + 1);
     }
 
+    // main loop
     for (let k = 0; k < 40; k++) {
         const newCounts = new Map();
-
         Array.from(counts.entries()).forEach(entry => {
             const [group, count] = entry;
 
@@ -32,25 +34,21 @@ function task1(data) {
             newCounts.set(next1, count + (newCounts.get(next1) || 0));
             newCounts.set(next2, count + (newCounts.get(next2) || 0));
         });
-
         counts = newCounts;
     }
 
-    let groupedCounts = Array.from(counts.entries()).reduce((acc, entry) => {
+    let lettersCount = Array.from(counts.entries()).reduce((acc, entry) => {
         const [[firstLetter, secondLetter], count] = entry;
         acc[firstLetter] = (acc[firstLetter] || 0) + count;
         acc[secondLetter] = (acc[secondLetter] || 0) + count;
         return acc;
     }, {});
 
-    groupedCounts = Object.entries(groupedCounts).reduce((acc, entry) => {
-        const [letter, count] = entry;
-        acc[letter] = Math.ceil(count / 2);
-        return acc;
-    }, {});
+    const sorted = Object.values(lettersCount)
+        .map(value => Math.ceil(value / 2))
+        .sort((a, b) => a - b);
 
-    const sortedGroupedCounts = Object.values(groupedCounts).sort((a, b) => a - b);
-    return sortedGroupedCounts[sortedGroupedCounts.length - 1] - sortedGroupedCounts[0];
+    return sorted[sorted.length - 1] - sorted[0];
 }
 
-new Day(2021, 15, isTest, false, task1);
+new Day(2021, 14, isTest, false, task1);
